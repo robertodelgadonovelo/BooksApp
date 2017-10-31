235a48c28fbe3cba67c76051e487b0abfbc0add5
Roberto Novelo <robertod.novelo@gmail.com>
2017-10-31 17:19:20 -0600

    Initial GraphCool Config

---
+++ app/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { AppRegistry } from "react-native";
+
+import {
+  ApolloProvider,
+  ApolloClient,
+  createNetworkInterface
+} from "react-apollo";
+import {
+  SubscriptionClient,
+  addGraphQLSubscriptions
+} from "subscriptions-transport-ws";
+
+const wsClient = new SubscriptionClient(
+  `wss://subscriptions.graph.cool/v1/YOURPROJECTID`,
+  {
+    reconnect: true
+  }
+);
+
+const networkInterface = createNetworkInterface({
+  uri: `https://api.graph.cool/simple/v1/YOURPROJECTID`
+});
+
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+  networkInterface,
+  wsClient
+);
+
+const client = new ApolloClient({
+  networkInterface: networkInterfaceWithSubscriptions
+});
+
+import App from "./app";
+
+const ApolloApp = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+AppRegistry.registerComponent("RNGraphCool", () => ApolloApp);
